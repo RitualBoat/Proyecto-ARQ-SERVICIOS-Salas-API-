0561790a232957810ab338cbf41416b79bec5678
jntellez <juantellez916@gmail.com>
2026-05-17 21:50:46 -0600

    Crea vistas para salas, mantenimientos, actividades y materiales en MongoDB

---
+++ db/vistas.mongodb.js
@@ -0,0 +1,74 @@
+use("SalasMantenimiento");
+
+db.createView("salasView", "salas", [
+  {
+    $project: {
+      _id: 0,
+      idSala: { $toString: "$_id" },
+      nombre: 1,
+      ubicacion: 1,
+      capacidad: 1,
+      estatus: 1,
+      tipo: 1,
+      idDepartamento: { $toString: "$idDepartamento" },
+    },
+  },
+]);
+
+db.createView("mantenimientosView", "mantenimientos", [
+  {
+    $project: {
+      _id: 0,
+      idMantenimiento: { $toString: "$_id" },
+      estatus: 1,
+      fechaInicio: 1,
+      fechaFin: 1,
+      idSala: { $toString: "$idSala" },
+      actividades: {
+        $map: {
+          input: "$actividades",
+          as: "idActividad",
+          in: { $toString: "$$idActividad" },
+        },
+      },
+    },
+  },
+]);
+
+db.createView("actividadesView", "actividades", [
+  {
+    $project: {
+      _id: 0,
+      idActividad: { $toString: "$_id" },
+      nombre: 1,
+      descripcion: 1,
+      completado: 1,
+      observaciones: 1,
+      idTecnico: { $toString: "$idTecnico" },
+      materiales: {
+        $map: {
+          input: "$materiales",
+          as: "material",
+          in: {
+            idMaterial: { $toString: "$$material.idMaterial" },
+            cantidad: "$$material.cantidad",
+          },
+        },
+      },
+    },
+  },
+]);
+
+db.createView("materialesView", "materiales", [
+  {
+    $project: {
+      _id: 0,
+      idMaterial: { $toString: "$_id" },
+      nombre: 1,
+      descripcion: 1,
+      tipo: 1,
+      unidadMedida: 1,
+      cantidadUnidades: 1,
+    },
+  },
+]);
